e54ee1835faaaf2efbb809f113b493dce6a4d818
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2024-03-15 16:02:15 +0100

    update demo

---
+++ browser-test/README.md
@@ -0,0 +1,17 @@
+# oracle-factory sandbox
+
+This project is a demo of the `@iexec/iexec-oracle-factory-wrapper` SDK.
+
+Browse the documentation at <https://tools.docs.iex.ec/tools/oracle-factory>.
+
+## install the project
+
+```sh
+npm ci
+```
+
+## start hacking
+
+```sh
+npm run dev
+```
--- browser-test/src/index.js
+++ browser-test/src/index.js
@@ -1,315 +1,302 @@
-import { IExecOracleFactory, IExecOracleReader, utils } from '../../dist';
-import './styles.css';
+import {
+  IExecOracleFactory,
+  IExecOracleReader,
+  utils,
+} from '@iexec/iexec-oracle-factory-wrapper';
+
+import {
+  getOutput,
+  formatObservableData,
+  formatObservableError,
+  setInitError,
+} from './helpers.js';
 
 const init = async () => {
   try {
     let ethProvider;
 
     if (window.ethereum) {
-      console.log('using default provider');
+      console.log('using default provider window.ethereum');
       ethProvider = window.ethereum;
     } else {
-      throw Error('Missing MetaMask');
+      throw Error('Missing injected provider `window.ethereum`');
     }
 
     await ethProvider.enable();
 
-    const factory = new IExecOracleFactory(ethProvider);
+    const oracleFactory = new IExecOracleFactory(ethProvider);
 
     document
       .getElementById('test-params-button')
-      .addEventListener('click', () => {
-        const out = document.getElementById('test-params-out');
-        out.value = '';
-        out.classList.remove('error');
-        utils
-          .testRawParams({
-            apiKey: document.getElementById('test-params-apikey-input').value,
-            url: document.getElementById('test-params-url-input').value,
-            method: document.getElementById('test-params-method-input').value,
-            body: document.getElementById('test-params-body-input').value,
-            JSONPath: document.getElementById('test-params-jsonpath-input')
-              .value,
-            dataType: document.getElementById('test-params-datatype-input')
-              .value,
-            headers:
-              (document.getElementById('test-params-headers-input').value &&
-                JSON.parse(
-                  document.getElementById('test-params-headers-input').value,
-                )) ||
-              {},
-          })
-          .then((res) => {
-            out.value = res;
-          })
-          .catch((e) => {
-            out.classList.add('error');
-            out.value = e.toString();
-          });
+      .addEventListener('click', async () => {
+        const out = getOutput('test-params-out');
+
+        const rawParams = {
+          apiKey: document.getElementById('test-params-apikey-input').value,
+          url: document.getElementById('test-params-url-input').value,
+          method: document.getElementById('test-params-method-input').value,
+          body: document.getElementById('test-params-body-input').value,
+          JSONPath: document.getElementById('test-params-jsonpath-input').value,
+          dataType: document.getElementById('test-params-datatype-input').value,
+          headers:
+            (document.getElementById('test-params-headers-input').value &&
+              JSON.parse(
+                document.getElementById('test-params-headers-input').value
+              )) ||
+            {},
+        };
+
+        try {
+          const res = await utils.testRawParams(rawParams);
+          out.success(res);
+        } catch (e) {
+          out.error(e.toString());
+        }
       });
 
     document
       .getElementById('create-oracle-button')
       .addEventListener('click', () => {
-        const out = document.getElementById('create-oracle-out');
-        out.value = '';
-        out.classList.remove('error');
-        factory
-          .createOracle({
-            apiKey: document.getElementById('create-oracle-apikey-input').value,
-            url: document.getElementById('create-oracle-url-input').value,
-            method: document.getElementById('create-oracle-method-input').value,
-            body: document.getElementById('create-oracle-body-input').value,
-            JSONPath: document.getElementById('create-oracle-jsonpath-input')
-              .value,
-            dataType: document.getElementById('create-oracle-datatype-input')
-              .value,
-            headers:
-              (document.getElementById('create-oracle-headers-input').value &&
-                JSON.parse(
-                  document.getElementById('create-oracle-headers-input').value,
-                )) ||
-              {},
-          })
-          .subscribe({
-            error: (e) => {
-              out.classList.add('error');
-              out.value += 'ERROR\n';
-              out.value += e.toString();
-              if (e.originalError) {
-                out.value += `\n${e.originalError.toString()}`;
-              }
-            },
-            next: (value) => {
-              const { message, ...rest } = value;
-              out.value += `${message}\n${Object.entries(rest)
-                .map(([k, v]) => `${k}: ${JSON.stringify(v)}`)
-                .join('\n')}${Object.entries(rest).length > 0 ? '\n\n' : '\n'}`;
-            },
-            complete: () => {
-              out.value += 'COMPLETE';
-            },
-          });
+        const out = getOutput('create-oracle-out');
+
+        const rawParams = {
+          apiKey: document.getElementById('create-oracle-apikey-input').value,
+          url: document.getElementById('create-oracle-url-input').value,
+          method: document.getElementById('create-oracle-method-input').value,
+          body: document.getElementById('create-oracle-body-input').value,
+          JSONPath: document.getElementById('create-oracle-jsonpath-input')
+            .value,
+          dataType: document.getElementById('create-oracle-datatype-input')
+            .value,
+          headers:
+            (document.getElementById('create-oracle-headers-input').value &&
+              JSON.parse(
+                document.getElementById('create-oracle-headers-input').value
+              )) ||
+            {},
+        };
+
+        oracleFactory.createOracle(rawParams).subscribe({
+          error: (e) => {
+            out.error(formatObservableError(e));
+          },
+          next: (data) => {
+            out.info(formatObservableData(data));
+          },
+          complete: () => {
+            out.success('COMPLETE');
+          },
+        });
       });
 
     document
       .getElementById('update-oracle-from-params-button')
       .addEventListener('click', () => {
-        const out = document.getElementById('update-oracle-from-params-out');
-        out.value = '';
-        out.classList.remove('error');
-        factory
-          .updateOracle(
-            {
-              dataset: document.getElementById(
-                'update-oracle-from-params-dataset-input',
-              ).value,
-              url: document.getElementById(
-                'update-oracle-from-params-url-input',
-              ).value,
-              method: document.getElementById(
-                'update-oracle-from-params-method-input',
-              ).value,
-              body: document.getElementById(
-                'update-oracle-from-params-body-input',
-              ).value,
-              JSONPath: document.getElementById(
-                'update-oracle-from-params-jsonpath-input',
-              ).value,
-              dataType: document.getElementById(
-                'update-oracle-from-params-datatype-input',
-              ).value,
-              headers:
-                (document.getElementById(
-                  'update-oracle-from-params-headers-input',
-                ).value &&
-                  JSON.parse(
-                    document.getElementById(
-                      'update-oracle-from-params-headers-input',
-                    ).value,
-                  )) ||
-                {},
-            },
-            { workerpool: '0x0e7bc972c99187c191a17f3cae4a2711a4188c3f' }, // bellecour prod pool
-          )
-          .subscribe({
-            error: (e) => {
-              out.classList.add('error');
-              out.value += 'ERROR\n';
-              out.value += e.toString();
-              if (e.originalError) {
-                out.value += `\n${e.originalError.toString()}`;
-              }
-            },
-            next: (value) => {
-              const { message, ...rest } = value;
-              out.value += `${message}\n${Object.entries(rest)
-                .map(([k, v]) => `${k}: ${JSON.stringify(v)}`)
-                .join('\n')}${Object.entries(rest).length > 0 ? '\n\n' : '\n'}`;
-            },
-            complete: () => {
-              out.value += 'COMPLETE';
-            },
-          });
+        const out = getOutput('update-oracle-from-params-out');
+
+        const paramSet = {
+          dataset: document.getElementById(
+            'update-oracle-from-params-dataset-input'
+          ).value,
+          url: document.getElementById('update-oracle-from-params-url-input')
+            .value,
+          method: document.getElementById(
+            'update-oracle-from-params-method-input'
+          ).value,
+          body: document.getElementById('update-oracle-from-params-body-input')
+            .value,
+          JSONPath: document.getElementById(
+            'update-oracle-from-params-jsonpath-input'
+          ).value,
+          dataType: document.getElementById(
+            'update-oracle-from-params-datatype-input'
+          ).value,
+          headers:
+            (document.getElementById('update-oracle-from-params-headers-input')
+              .value &&
+              JSON.parse(
+                document.getElementById(
+                  'update-oracle-from-params-headers-input'
+                ).value
+              )) ||
+            {},
+        };
+
+        oracleFactory.updateOracle(paramSet).subscribe({
+          error: (e) => {
+            out.error(formatObservableError(e));
+          },
+          next: (data) => {
+            out.info(formatObservableData(data));
+          },
+          complete: () => {
+            out.success('COMPLETE');
+          },
+        });
       });
 
     document
       .getElementById('update-oracle-from-cid-button')
       .addEventListener('click', () => {
-        const out = document.getElementById('update-oracle-from-cid-out');
-        out.value = '';
-        out.classList.remove('error');
-        factory
-          .updateOracle(
-            document.getElementById('update-oracle-from-cid-cid-input').value,
-            { workerpool: '0x0e7bc972c99187c191a17f3cae4a2711a4188c3f' }, // bellecour prod pool
-          )
+        const out = getOutput('update-oracle-from-cid-out');
+
+        const paramSetCid = document.getElementById(
+          'update-oracle-from-cid-cid-input'
+        ).value;
+
+        oracleFactory.updateOracle(paramSetCid).subscribe({
+          error: (e) => {
+            out.error(formatObservableError(e));
+          },
+          next: (data) => {
+            out.info(formatObservableData(data));
+          },
+          complete: () => {
+            out.success('COMPLETE');
+          },
+        });
+      });
+
+    document
+      .getElementById('update-oracle-x-chain-button')
+      .addEventListener('click', () => {
+        const out = getOutput('update-oracle-x-chain-out');
+
+        const paramSetCid = document.getElementById(
+          'update-oracle-x-chain-cid-input'
+        ).value;
+
+        const targetBlockchains = document.getElementById(
+          'update-oracle-x-chain-target-input'
+        ).value;
+
+        oracleFactory
+          .updateOracle(paramSetCid, {
+            targetBlockchains: JSON.parse(targetBlockchains),
+          })
           .subscribe({
             error: (e) => {
-              out.classList.add('error');
-              out.value += 'ERROR\n';
-              out.value += e.toString();
-              if (e.originalError) {
-                out.value += `\n${e.originalError.toString()}`;
-              }
+              out.error(formatObservableError(e));
             },
-            next: (value) => {
-              const { message, ...rest } = value;
-              out.value += `${message}\n${Object.entries(rest)
-                .map(([k, v]) => `${k}: ${JSON.stringify(v)}`)
-                .join('\n')}${Object.entries(rest).length > 0 ? '\n\n' : '\n'}`;
+            next: (data) => {
+              out.info(formatObservableData(data));
             },
             complete: () => {
-              out.value += 'COMPLETE';
+              out.success('COMPLETE');
             },
           });
       });
 
     document
       .getElementById('read-oracle-from-params-button')
-      .addEventListener('click', () => {
-        const out = document.getElementById('read-oracle-from-params-out');
-        out.value = '';
-        out.classList.remove('error');
-        factory
-          .readOracle({
-            dataset: document.getElementById(
-              'read-oracle-from-params-dataset-input',
-            ).value,
-            url: document.getElementById('read-oracle-from-params-url-input')
-              .value,
-            method: document.getElementById(
-              'read-oracle-from-params-method-input',
-            ).value,
-            body: document.getElementById('read-oracle-from-params-body-input')
-              .value,
-            JSONPath: document.getElementById(
-              'read-oracle-from-params-jsonpath-input',
-            ).value,
-            dataType: document.getElementById(
-              'read-oracle-from-params-datatype-input',
-            ).value,
-            headers:
-              (document.getElementById('read-oracle-from-params-headers-input')
-                .value &&
-                JSON.parse(
-                  document.getElementById(
-                    'read-oracle-from-params-headers-input',
-                  ).value,
-                )) ||
-              {},
-          })
-          .then((res) => {
-            out.value += JSON.stringify(res);
-          })
-          .catch((e) => {
-            out.classList.add('error');
-            out.value += e.toString();
-          });
+      .addEventListener('click', async () => {
+        const out = getOutput('read-oracle-from-params-out');
+
+        const paramSet = {
+          dataset: document.getElementById(
+            'read-oracle-from-params-dataset-input'
+          ).value,
+          url: document.getElementById('read-oracle-from-params-url-input')
+            .value,
+          method: document.getElementById(
+            'read-oracle-from-params-method-input'
+          ).value,
+          body: document.getElementById('read-oracle-from-params-body-input')
+            .value,
+          JSONPath: document.getElementById(
+            'read-oracle-from-params-jsonpath-input'
+          ).value,
+          dataType: document.getElementById(
+            'read-oracle-from-params-datatype-input'
+          ).value,
+          headers:
+            (document.getElementById('read-oracle-from-params-headers-input')
+              .value &&
+              JSON.parse(
+                document.getElementById('read-oracle-from-params-headers-input')
+                  .value
+              )) ||
+            {},
+        };
+
+        try {
+          const res = await oracleFactory.readOracle(paramSet);
+          out.success(JSON.stringify(res));
+        } catch (e) {
+          out.error(e.toString());
+        }
       });
 
     document
       .getElementById('read-oracle-from-cid-button')
-      .addEventListener('click', () => {
-        const out = document.getElementById('read-oracle-from-cid-out');
-        out.value = '';
-        out.classList.remove('error');
-        factory
-          .readOracle(
-            document.getElementById('read-oracle-from-cid-cid-input').value,
-          )
-          .then((res) => {
-            out.value += JSON.stringify(res);
-          })
-          .catch((e) => {
-            out.classList.add('error');
-            out.value += e.toString();
-          });
+      .addEventListener('click', async () => {
+        const out = getOutput('read-oracle-from-cid-out');
+
+        const paramSetCid = document.getElementById(
+          'read-oracle-from-cid-cid-input'
+        ).value;
+
+        try {
+          const res = await oracleFactory.readOracle(paramSetCid);
+          out.success(JSON.stringify(res));
+        } catch (e) {
+          out.error(e.toString());
+        }
       });
 
     document
       .getElementById('read-oracle-from-oracleid-button')
-      .addEventListener('click', () => {
-        const out = document.getElementById('read-oracle-from-oracleid-out');
-        out.value = '';
-        out.classList.remove('error');
-        factory
-          .readOracle(
-            document.getElementById('read-oracle-from-oracleid-oracleid-input')
-              .value,
-            {
-              dataType: document.getElementById(
-                'read-oracle-from-oracleid-datatype-input',
-              ).value,
-            },
-          )
-          .then((res) => {
-            out.value += JSON.stringify(res);
-          })
-          .catch((e) => {
-            out.classList.add('error');
-            out.value += e.toString();
-          });
+      .addEventListener('click', async () => {
+        const out = getOutput('read-oracle-from-oracleid-out');
+
+        const oracleId = document.getElementById(
+          'read-oracle-from-oracleid-oracleid-input'
+        ).value;
+
+        const dataType = document.getElementById(
+          'read-oracle-from-oracleid-datatype-input'
+        ).value;
+
+        try {
+          const res = await oracleFactory.readOracle(oracleId, { dataType });
+          out.success(JSON.stringify(res));
+        } catch (e) {
+          out.error(e.toString());
+        }
       });
 
     document
       .getElementById('read-x-chain-oracle-from-oracleid-button')
       .addEventListener('click', async () => {
-        const provider =
-          document.getElementById(
-            'read-x-chain-oracle-from-oracleid-provider-input',
-          ).value || 'injected';
-        const out = document.getElementById(
-          'read-x-chain-oracle-from-oracleid-out',
-        );
-        out.value = '';
-        out.classList.remove('error');
-        out.value += `reading value with provider ${provider}\n`;
-        try {
-          const reader = new IExecOracleReader(
-            provider === 'injected' ? ethProvider : provider,
-          );
-          const res = await reader.readOracle(
-            document.getElementById(
-              'read-x-chain-oracle-from-oracleid-oracleid-input',
-            ).value,
-            {
-              dataType: document.getElementById(
-                'read-x-chain-oracle-from-oracleid-datatype-input',
-              ).value,
-            },
-          );
+        const out = getOutput('read-x-chain-oracle-from-oracleid-out');
+        const provider = document.getElementById(
+          'read-x-chain-oracle-from-oracleid-provider-input'
+        ).value;
 
-          out.value += JSON.stringify(res);
+        const oracleId = document.getElementById(
+          'read-x-chain-oracle-from-oracleid-oracleid-input'
+        ).value;
+
+        const dataType = document.getElementById(
+          'read-x-chain-oracle-from-oracleid-datatype-input'
+        ).value;
+
+        try {
+          const oracleReader = provider
+            ? new IExecOracleReader(provider)
+            : oracleFactory;
+
+          const res = await oracleReader.readOracle(oracleId, { dataType });
+          out.success(JSON.stringify(res));
         } catch (e) {
-          out.classList.add('error');
-          out.value += e.toString();
+          out.error(e.toString());
         }
       });
 
     console.log('initialized');
   } catch (e) {
-    console.log('failed to init', e);
+    console.log('failed to initialize', e);
+    setInitError(`Failed to initialize: ${e.message}`);
   }
 };
 
