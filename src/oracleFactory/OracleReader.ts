64d68d25cb92a2f3bd89c5f1d6de6e660d1ee592
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2024-07-26 17:12:22 +0200

    add support for AbstractSigner and AbstractProvider

--- src/oracleFactory/OracleReader.ts
+++ src/oracleFactory/OracleReader.ts
@@ -51,6 +51,7 @@ class IExecOracleReader {
     ethProviderOrNetwork:
       | Web3ReadOnlyProvider
       | Web3SignerProvider
+      | AbstractProvider
       | Eip1193Provider
       | string
       | number = 134,
